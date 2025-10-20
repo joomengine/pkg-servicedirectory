704b9dd79278d0ae63df2dd47fd96b493ce4f28a
aB0t <robot@vdm.io>
2025-10-20 18:22:57 +0000

    Update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# pkg-servicedirectory
+# Service Directory Package (v6.0.0)
+
+The Joomla Service Directory Package that bundles JSD Component and all the plugins and features available into one package for easy install and update of your JSD system for Joomla 6.
+
+## Details
+
+- Packager: [Vast Development Method](https://dev.vdm.io/)
+- Author: [Llewellyn van der Merwe](https://io.vdm.dev/)
+- Creation Date: October 2025
+- Max Joomla Version: J6.1
+- Minimum Joomla Version: J6.0
+
+## Extensions
+
+- [joomla/Service-Directory](https://git.vdm.robot/joomla/Service-Directory) [6.x](https://git.vdm.robot/joomla/Service-Directory/archive/6.x.zip)
+> 6.x
+- [joomla/plg_privacy_servicedirectory](https://git.vdm.robot/joomla/plg_privacy_servicedirectory) [6.x](https://git.vdm.robot/joomla/plg_privacy_servicedirectory/archive/6.x.zip)
+> 6.x
+
+### Copyright (2015)
+> Copyright (C) 2015 Llewellyn van der Merwe. All rights reserved.
+### License
+> GNU General Public License version 2; see LICENSE
 
---
+++ install_servicedirectory.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package    Joomla.Service.Directory
+ *
+ * @created    30th April, 2015
+ * @author     Llewellyn van der Merwe <https://dev.vdm.io>
+ * @git        Joomla Service Directory <https://git.vdm.dev/joomla/Service-Directory>
+ * @copyright  Copyright (C) 2015 Vast Development Method. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseInterface;
+
+/**
+ * Script File of Componentbuilder Package
+ */
+class pkg_servicedirectoryInstallerScript
+{
+	/**
+	 * Called after any type of action
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function postflight($type, $parent)
+	{
+		// only run these if we have an update
+		if ('update' == $type)
+		{
+			// update the update server location
+			$this->updateServerLocation();
+		}
+	}
+
+	/**
+	 * Update server location
+	 *
+	 * @return  void
+	 */
+	protected function updateServerLocation()
+	{
+		$location = "https://raw.githubusercontent.com/joomengine/Joomla-Service-Directory/refs/heads/6.x/update_server.xml";
+		$elements = ['pkg_servicedirectory', 'com_servicedirectory'];
+		$db = Factory::getContainer()->get(DatabaseInterface::class);
+
+		// Get the Package Update Site Details
+		foreach ($elements as $element)
+		{
+			if (($sites = $this->getUpdateSites($element, $db)) !== null)
+			{
+				foreach ($sites as $site)
+				{
+					if ($site->location !== $location)
+					{
+						// Update the update site location
+						$site->location = $location;
+						$db->updateObject('#__update_sites', $site, 'update_site_id');
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Get Update Sites
+	 *
+	 * @return  array|null
+	 */
+	protected function getUpdateSites(string $element, DatabaseInterface $db): ?array
+	{
+		// Get the Package Update Site Details
+		$query = $db->getQuery(true);
+		$query->select($db->quoteName(array('s.location', 's.update_site_id')));
+		$query->from($db->quoteName('#__update_sites', 's'));
+		$query->join('LEFT', $db->quoteName('#__update_sites_extensions', 'u') . ' ON ' . $db->quoteName('s.update_site_id') . ' = ' . $db->quoteName('u.update_site_id'));
+		$query->join('LEFT', $db->quoteName('#__extensions', 'e') . ' ON ' . $db->quoteName('u.extension_id') . ' = ' . $db->quoteName('e.extension_id'));
+		$query->where($db->quoteName('e.element') . ' = ' . $db->quote($element));
+		$db->setQuery($query);
+		$db->execute();
+
+		if ($db->getNumRows())
+		{
+			return $db->loadObjectList();
+		}
+		return null;
+	}
+}
+
